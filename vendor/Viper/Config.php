cc2bc4f8eb0945db1b53c667a17ef2589caf198a
BlackFoxFire <blackfoxfire31@gmail.com>
2016-12-23 00:41:09 +0100

    Ajout de la classe Config.

---
+++ vendor/Viper/Config.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+*
+* Config.php
+* @Auteur : Christophe Dufour
+*
+* 
+*
+*/
+
+namespace Viper;
+
+class Config extends ApplicationComponent {
+	
+	/*
+		Les attributs.
+		--------------
+	*/
+	
+	// Tableau des paramètres de l'application.
+	protected $vars = array();
+	
+	/*
+		Les méthodes.
+		-------------
+	*/
+	
+	// Retourne la valeur d'un parametre de l'application.
+	public function get($var) {
+		if(!$this->vars) {
+			$xml = new \DOMDocument;
+			$xml->load(SRC . $this->name . DS . "resources" . DS . "config" . DS . "app.xml");
+			
+			$elements = $xml->getElementsByTagName("define");
+			
+			foreach($elements as $element) {
+				$this->vars[$element->getAttribute("var")] = $element->getAttribute("value");
+			}
+		}
+		
+		if(isset($this->vars[$var])) {
+			return $this->vars[$var];
+		}
+		
+		return null;
+	}
+	
+}
