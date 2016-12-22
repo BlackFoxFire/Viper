42ba9b58dbd4c842dc408f03f9199918a9378a91
BlackFoxFire <blackfoxfire31@gmail.com>
2016-12-22 23:22:34 +0100

    Ajout de la classe Entity.

---
+++ vendor/Viper/Entity.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+*
+* Entity.php
+* @Auteur : Christophe Dufour
+*
+* Classe de base pour toutes les entités.
+*
+*/
+
+namespace Viper;
+
+abstract class Entity {
+	
+	// Utilisation du trait Hydrator.
+	use Hydrator;
+	
+	/*
+		Les attributs.
+		--------------
+	*/
+	
+	// Identifiant unique de l'entité.
+	protected $id;
+	
+	// Tableau des éventuelles erreurs.
+	protected $errors = array();
+	
+	/*
+		Constructeur.
+		-------------
+	*/
+	
+	// Constructeur de classe.
+	public function construct(array $data = array()) {
+		if(!empty($data)) {
+			$this->hydratation($data);
+		}
+	}
+	
+	/*
+		Les getters.
+		------------
+	*/
+	
+	// Retourne la valeur de l'attribut $id.
+	public function id() {
+		return $this->id;
+	}
+	
+	// Retourne la valeur de l'attribut $errors.
+	public function errors() {
+		return $this->errors;
+	}
+	
+	/*
+		Les setters.
+		------------
+	*/
+	
+	// Modifie la valeur de l'attribut $id.
+	public function setId($value) {
+		if(is_numeric($value) && (int) $value > 0) {
+			$this->id = (int) $value;
+		}
+	}
+	
+	/*
+		Les méthodes.
+		-------------
+	*/
+	
+	// Retourne true si il y a des erreurs. Sinon false.
+	public function hasErrors() {
+		return !empty($this->errors);
+	}
+	
+	// Retourne true si c'est un nouvel objet. Sinon false.
+	public function isNew() {
+		return empty($this->id);
+	}
+	
+}
