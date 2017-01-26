7ff7eca0acfe83514942026efc14a9a3ed5d5b67
BlackFoxFire <blackfoxfire31@gmail.com>
2017-01-26 11:58:52 +0100

    Ajout d'un paramètre d'un méthode dans PDOFactory.

--- vendor/Viper/PDOFactory.php
+++ vendor/Viper/PDOFactory.php
@@ -19,8 +19,8 @@ class PDOFactory {
 	*/
 	
 	// Etablit la connexion avec la base de données.
-	public static function mysqlConnexion() {echo "OOOOOOOOO";
-		$db = new \PDO("mysql:host=localhost;dbname=news", "root", "");
+	public static function mysqlConnexion($data) {
+		$db = new \PDO("mysql:host=localhost;dbname=" . $data['db'], $data['login'], $data['password']);
 		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 		
 		return $db;
