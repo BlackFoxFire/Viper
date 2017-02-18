04ffe3aa4bf29206afa9636c98607ad7f86e4a1d
BlackFoxFire <blackfoxfire31@gmail.com>
2017-02-18 11:43:15 +0100

    Cangement sur User.php

--- vendor/Viper/User.php
+++ vendor/Viper/User.php
@@ -13,14 +13,30 @@ namespace Viper;
 
 class User extends ApplicationComponent {
 	
+	/*
+		Constructeur.
+		-------------
+	*/
+	
+	// Constructeur de classe.
+	public function __construct() {
+		session_set_cookie_params(0, dirname($_SERVER['SCRIPT_NAME']), "", false, true);
+		session_start();
+	}
+	
+	/*
+		Les méthodes.
+		-------------
+	*/
+	
 	// Ajoute ou modifie une variable de session utilisateur.
-	protected function set($key, $value = "") {
+	public function set($key, $value = "") {
 		$_SESSION[$key] = $value;
 	}
 	
 	// Renvoie la valeur d'une variable de session utilisateur.
 	// Si celui ci n'existe pas, c'est returnValue qui est renvoyé.
-	protected function get($key, $returnValue = false) {
+	public function get($key, $returnValue = false) {
 		if(isset($_SESSION[$key]))
 			return $_SESSION[$key];
 		
@@ -29,7 +45,7 @@ class User extends ApplicationComponent {
 	
 	// Vérifie si un utilisateur est authentifié.
 	// Retourne true si oui. Sinon false.
-	protected function isAuthenticated() {
+	public function isAuthenticated() {
 		if(isset($_SESSION['isAuthenticated']) && $_SESSION['isAuthenticated'] === true)
 			return true;
 		
@@ -37,7 +53,7 @@ class User extends ApplicationComponent {
 	}
 	
 	// Modifie le statut d'authentifié.
-	protected function setAuthenticated($value = true) {
+	public function setAuthenticated($value = true) {
 		if(!is_bool($value)) {
 			throw new \InvalidArgumentException(
 				"La valeur spécifiée à la méthode User::setAuthenticated() doit être un boolean");
@@ -47,14 +63,14 @@ class User extends ApplicationComponent {
 	}
 	
 	// Définit un message pour l'utilisateur.
-	protected function setMessage($value) {
+	public function setMessage($value) {
 		if(!empty($value) && is_string($value)) {
 			$_SESSION['hasMessage'] = $value;
 		}
 	}
 	
 	// Retourne un message utilisateur si celui-ci existe.
-	protected function getMessage() {
+	public function getMessage() {
 		if(isset($_SESSION['hasMessage'])) {
 			$message = $_SESSION['hasMessage'];
 			unset($_SESSION['hasMessage']);
@@ -65,7 +81,7 @@ class User extends ApplicationComponent {
 	
 	// Vérifie si un message est présent.
 	// Renvoie true si oui. Sinon false.
-	protected function hasMessage() {
+	public function hasMessage() {
 		if(isset($_SESSION['hasMessage']))
 			return true;
 		
