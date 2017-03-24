72ae025ef7c270eec7ce7420cb54fb8c11241e83
BlackFoxFire <blackfoxfire31@gmail.com>
2017-03-24 23:00:24 +0100

    Modification du fichier HTTPRequest et enregistrement au format unix.

--- vendor/Viper/HTTPRequest.php
+++ vendor/Viper/HTTPRequest.php
@@ -21,17 +21,17 @@ class HTTPRequest extends ApplicationComponent {
 	/* Fonction sur la superglobal $_GET. */
 	
 	// Vérifie si un élément existe dans le tableau $_GET.
-	public function issetGet($key) {
+	public function getKeyExists($key) {
 		return isset($_GET[$key]);
 	}
 	
 	// Vérifie si un élément est vide dans $_GET.
-	public function emptyGet($key) {
+	public function getKeyEmpty($key) {
 		return empty($_GET[$key]);
 	}
 	
 	// Retourne la valeur d'un des éléments de $_GET.
-	public function get($key, $returnValue = false) {
+	public function getFromGet($key, $returnValue = false) {
 		if(isset($_GET[$key])) {
 			return $_GET[$key];
 		}
@@ -42,17 +42,17 @@ class HTTPRequest extends ApplicationComponent {
 	/* Fonction sur la superglobal $_POST. */
 	
 	// Vérifie si un élément existe dans le tableau $_POST.
-	public function issetPost($key) {
+	public function postKeyExists($key) {
 		return isset($_POST[$key]);
 	}
 	
 	// Vérifie si un élément est vide dans $_POST.
-	public function emptyPost($key) {
+	public function postKeyEmpty($key) {
 		return empty($_POST[$key]);
 	}
 	
 	// Retourne la valeur d'un des éléments de $_POST.
-	public function post($key, $returnValue = false) {
+	public function getFromPost($key, $returnValue = false) {
 		if(isset($_POST[$key])) {
 			return $_POST[$key];
 		}
@@ -60,10 +60,20 @@ class HTTPRequest extends ApplicationComponent {
 		return $returnValue;
 	}
 	
+	// Teste si un formulaire a été envoyé.
+	// Retourne true si c'est le cas, sinon false.
+	public function formIsSubmit() {
+		if($_SERVER['REQUEST_METHOD'] == "POST" || isset($_POST['submit'])) {
+			return true;
+		}
+		
+		return false;
+	}
+	
 	/* Fonction sur la superglobal $_COOKIE. */
 	
 	// Vérifie si un élément existe dans le tableau $_COOKIE.
-	public function issetCookie($key) {
+	public function cookieExists($key) {
 		return isset($_COOKIE[$key]);
 	}
 	
@@ -85,10 +95,7 @@ class HTTPRequest extends ApplicationComponent {
 	
 	// Retourne la valeur de l'élément REQUEST_URI de la super global _SERVER.
 	public function requestURI() {
-		$baseURI = dirname($_SERVER['SCRIPT_NAME']);
-		return str_replace($baseURI, '', $_SERVER['REQUEST_URI']);
-		
-		// return $_SERVER['REQUEST_URI'];
+		return str_replace(BASE_URI, '', $_SERVER['REQUEST_URI']);
 	}
 	
 }
